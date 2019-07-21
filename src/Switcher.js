b590b670ba821a29cade8dcd0257fefdde6ea27c
Alina Voronina <skorokhodova.a5@gmail.com>
2019-07-21 11:07:11 +0300

    Отображение списка детей компонента Switcher

--- src/Switcher.js
+++ src/Switcher.js
@@ -1,12 +1,32 @@
-import {Component} from 'react';
+import React, { Component } from 'react';
 import './Switcher.css';
 
 // Для работы этой компоненты нужно использовать методы React.Children.toArray
 // а так же работать с child.type.name и child.type.displayName
 
 class Switcher extends Component {
+  componentDidMount() {
+    React.Children.forEach(this.props.children, child => {
+      console.log('name=', child.type.name);
+    });
+  }
+
   render() {
-    return null;
+    const { children } = this.props;
+
+    return (
+      <div>
+        <nav>
+          <ul className="component-list">
+            {React.Children.map(children, child => (
+              <li className="component-list__name">{child.type.name}</li>
+            ))}
+          </ul>
+        </nav>
+        <hr />
+        <div className="component-wrapper" />
+      </div>
+    );
   }
 }
 
