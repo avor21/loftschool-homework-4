c4765d582c52c3a4269337abad43ce3ddab916b0
Alina Voronina <skorokhodova.a5@gmail.com>
2019-07-22 10:26:24 +0300

    Генерация компонента по выбранному имени

--- src/Switcher.js
+++ src/Switcher.js
@@ -5,26 +5,44 @@ import './Switcher.css';
 // а так же работать с child.type.name и child.type.displayName
 
 class Switcher extends Component {
+  state = {
+    selectedComponent: 0
+  };
+
   componentDidMount() {
     React.Children.forEach(this.props.children, child => {
       console.log('name=', child.type.name);
     });
   }
 
+  handleClickOnComponentName = event => {
+    const newValue = event.target.getAttribute('data-id');
+    this.setState({ selectedComponent: newValue });
+  };
+
   render() {
     const { children } = this.props;
+    const { selectedComponent } = this.state;
 
     return (
       <div>
         <nav>
           <ul className="component-list">
-            {React.Children.map(children, child => (
-              <li className="component-list__name">{child.type.displayName}</li>
+            {React.Children.map(children, (child, index) => (
+              <li
+                className="component-list__name"
+                data-id={index}
+                onClick={this.handleClickOnComponentName}
+              >
+                {child.type.displayName}
+              </li>
             ))}
           </ul>
         </nav>
         <hr />
-        <div className="component-wrapper" />
+        <div className="component-wrapper">
+          {React.Children.toArray(children)[selectedComponent]}
+        </div>
       </div>
     );
   }
