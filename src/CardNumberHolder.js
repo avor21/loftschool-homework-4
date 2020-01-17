94cca0f4fcb53692f417edf8ce1852839099bce3
Alina Voronina <skorokhodova.a5@gmail.com>
2020-01-18 00:31:15 +0300

    Создание компонента CardNumberHolder и инпута CardNumberInput

--- src/CardNumberHolder.js
+++ src/CardNumberHolder.js
@@ -1,10 +1,26 @@
-import {Component} from 'react';
+import React, {Component} from 'react';
+import CardNumberInput from './CardNumberInput'
 
 class CardNumberHolder extends Component {
   static displayName = 'Credit card number formatting';
 
+  state = {
+    cardNumber: ''
+  };
+
+  handleChangeNumberInput = cardNumber => {
+    this.setState({cardNumber});
+  };
+
   render() {
-    return null;
+    const {cardNumber} = this.state;
+
+    return (
+    <div>
+      <CardNumberInput cardNumber={cardNumber}
+                       onChange={this.handleChangeNumberInput} />
+    </div>
+    );
   }
 }
 
--- src/CardNumberInput.js
+++ src/CardNumberInput.js
@@ -1,8 +1,22 @@
-import {Component} from 'react';
+import React, { Component } from 'react';
 
 class CardNumberInput extends Component {
+
+  handleChangeNumberFormatting = event => {
+    const { onChange } = this.props;
+    const newValue = event.target.value;
+    onChange(newValue);
+  };
+
   render() {
-    return null;
+    const { cardNumber } = this.props;
+    return (
+      <div>
+        <input value={cardNumber}
+               onChange={this.handleChangeNumberFormatting}
+               placeholder="Введите номер карты" />
+      </div>
+    );
   }
 }
 
