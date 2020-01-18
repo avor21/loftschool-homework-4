b56be0e8487937d493c390909a3a7cffb045d819
Alina Voronina <skorokhodova.a5@gmail.com>
2020-01-18 22:57:25 +0300

    Подгон под тесты

--- src/ModalButton.js
+++ src/ModalButton.js
@@ -1,11 +1,41 @@
-import {Component} from 'react';
+import React, {Component} from 'react';
+import Modal from "./Modal";
+import './ModalButton.css';
 
 class ModalButton extends Component {
   static displayName = 'Modal window';
 
+  state = {
+    isModalShow: false
+  };
+
   render() {
-    return null;
+    const { isModalShow } = this.state;
+    const modal = isModalShow
+      && <Modal className="modal">
+        <div className="modal__fog">
+          <div className="modal__body">
+            <h2>Modal Window</h2>
+            <button onClick={this.hideModal}>Close</button>
+          </div>
+        </div>
+         </Modal>;
+
+    return (
+      <div>
+        <button onClick={this.showModal}>Click me!</button>
+        { modal }
+      </div>
+    );
   }
+
+  showModal = () => {
+    this.setState({ isModalShow: true });
+  };
+
+  hideModal = () => {
+    this.setState({ isModalShow: false });
+  };
 }
 
 export default ModalButton;
